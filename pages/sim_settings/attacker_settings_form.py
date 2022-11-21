28d1790f69999a0603ab2bc1347756b098e5dabf
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-11-21 12:34:21 -0600

    misc

---
+++ pages/sim_settings/attacker_settings_form.py
@@ -0,0 +1,80 @@
+
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.textinput import TextInput
+
+class AttackerInput(StackLayout):
+     def __init__(self, **kwargs):
+        super(AttackerInput, self).__init__(**kwargs)
+        # --
+        self.class_id = TextInput(
+            text="class",
+            size_hint=(0.1, 0.05),
+        )
+        self.add_widget(self.class_id, index=1)
+        # --
+        self.ap = TextInput(
+            text="ap",
+            size_hint=(0.1, 0.05),
+        )
+        self.add_widget(self.ap, index=2)
+        # --
+        self.aap = TextInput(
+            text="aap",
+            size_hint=(0.1, 0.05),
+        )
+        self.add_widget(self.aap, index=3)
+        # --
+        self.acc = TextInput()
+        self.add_widget(self.acc, index=4)
+        # --
+        self.acc_rate = TextInput()
+        self.add_widget(self.acc_rate, index=5)
+        # --
+        self.crit_rate = TextInput()
+        self.add_widget(self.crit_rate, index=6)
+        # --
+        self.monster_ap = TextInput()
+        self.add_widget(self.monster_ap, index=7)
+        # --
+        self.kama_damage = TextInput()
+        self.add_widget(self.kama_damage, index=8)
+        # --
+        self.demi_damage = TextInput()
+        self.add_widget(self.demi_damage, index=9)
+        # --
+        self.human_damage = TextInput()
+        self.add_widget(self.human_damage, index=10)
+        # --
+        self.other_damage = TextInput()
+        self.add_widget(self.other_damage, index=11)
+        # --
+        self.crit_damage = TextInput()
+        self.add_widget(self.crit_damage, index=12)
+        # --
+        self.back_damage = TextInput()
+        self.add_widget(self.back_damage, index=13)
+        # --
+        self.down_damage = TextInput()
+        self.add_widget(self.down_damage, index=14)
+        # --
+        self.air_damage = TextInput()
+        self.add_widget(self.air_damage, index=15)
+        # --
+        self.ap_combat_buffs = TextInput()
+        self.add_widget(self.ap_combat_buffs, index=16)
+        # --
+        self.crit_combat_buffs = TextInput()
+        self.add_widget(self.crit_combat_buffs, index=17)
+        # --
+        self.ap_debuffs = TextInput()
+        self.add_widget(self.ap_debuffs, index=18)
+        # --
+        self.acc_combat_buffs = TextInput()
+        self.add_widget(self.acc_combat_buffs, index=19)
+        # --
+        self.acc_debuffs = TextInput()
+        self.add_widget(self.acc_debuffs, index=20)
+        # --
+        self.human_damage_debuffs = TextInput()
+        self.add_widget(self.human_damage_debuffs, index=21)
+        # --
--- pages/sim_settings/sim_settings_screen.py
+++ pages/sim_settings/sim_settings_screen.py
@@ -3,8 +3,11 @@
 import os
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
 
 # local imports
+from .attacker_settings_form import AttackerInput
 
 # =============================================================================
 Builder.load_file(f"{os.getcwd()}\\pages\\sim_settings\\sim_settings_style.kv")
@@ -12,3 +15,19 @@ Builder.load_file(f"{os.getcwd()}\\pages\\sim_settings\\sim_settings_style.kv")
 
 class SimSettingsScreen(Screen):
     """Sim Settings Page"""
+
+    def __init__(self, **kwargs):
+        super(SimSettingsScreen, self).__init__(**kwargs)
+        # --
+        self.form = AttackerInput(
+            padding=50,
+            size_hint=(.8, 0.15),
+            width=900,
+            orientation="lr-tb",
+            spacing=[30, 30],
+        )
+        self.add_widget(self.form)
+        # --
+
+
+# ------------------
