418493351dfe622df9db32c789fc24999f12de39
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2023-07-11 17:06:19 -0500

    misc

--- main.py
+++ main.py
@@ -3,28 +3,49 @@
 import os
 import kivy
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import ScreenManager, ShaderTransition
 from kivy.lang import Builder
-from pages.sim_settings.sim_settings_screen import SimSettingsScreen
+from kivy.properties import ObjectProperty
 
 # local imports
 from pages.splash.splash_screen import SplashScreen
 from pages.login.login_screen import LoginScreen
 from pages.sim_settings.sim_settings_screen import SimSettingsScreen
+from pages.wiki.wiki import WikiScreen
 
 # =============================================================================
 
-Builder.load_file(f"{os.getcwd()}\\app_shell.kv")
+
+class AppShell(GridLayout):
+    """Root Widget"""
+    Builder.load_file(f"{os.getcwd()}\\app_shell.kv")
+    screen_manager = ObjectProperty(ScreenManager())
+
+    columns = 1
+    rows = 2
+    #
+
+    def __init__(self, **kwargs) -> None:
+        super(AppShell, self).__init__(**kwargs)
+        # self.screen_manager.add_widget(SplashScreen(name="SplashScreen"))
+        self.screen_manager.add_widget(LoginScreen(name="LoginScreen"))
+        self.screen_manager.add_widget(SimSettingsScreen(name="SimSettingsScreen"))
+        self.screen_manager.add_widget(WikiScreen(name="WikiScreen"))
+        #
+        self.ids.selector.values = [
+            screen.name for screen in self.screen_manager.screens
+        ]
+        self.ids.selector.bind(text=self.change_screen)
+
+    def change_screen(self, instance, value):
+        self.screen_manager.current = "SplashScreen"
 
 
 class ProjectYnix(App):
-    """Application"""
     def build(self):
-        s_man = ScreenManager()
-        s_man.add_widget(SplashScreen(name='SplashScreen'))
-        s_man.add_widget(LoginScreen(name='LoginScreen'))
-        s_man.add_widget(SimSettingsScreen(name='SimSettingsScreen'))
-        return s_man
+        return AppShell()
 
 
 if __name__ == "__main__":
